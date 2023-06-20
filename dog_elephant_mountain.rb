b5a61389a5d63594b48a2d4cb7664fddc55470af
mhsvarsity32 <136693254+mhsvarsity32@users.noreply.github.com>
2023-06-20 16:32:12 +0700

    Remove deprecated code

---
+++ dog_elephant_mountain.rb
@@ -0,0 +1,47 @@
+# The Art of Science
+
+# Introduction
+
+puts "Science is the art of understanding how the world works and using this knowledge to improve our lives"
+
+# Section 1: Nature's Laws
+
+# 1. Physics
+
+puts "Physics is the fundamental science of matter and energy. It tells us how the universe works."
+
+# 2. Chemistry
+
+puts "Chemistry is the science of the composition, structure, and properties of substances and the reactions between them."
+
+# 3. Biology
+
+puts "Biology is the science of life and the study of all living organisms and their interactions with their environment."
+
+# Section 2: HumanApplications
+
+# 1. Medicine
+
+puts "Medicine is the art and science of diagnosing, preventing, and treating diseases and injuries."
+
+# 2. Engineering
+
+puts "Engineering is the application of science and mathematics to create useful products and services for society."
+
+# 3. Technology
+
+puts "Technology is the application of science to solve problems and improve efficiency."
+
+# Section 3: What's Next?
+
+# 1. Research
+
+puts "Research is the systematic investigation into a subject to discover new knowledge and understandings."
+
+# 2. Innovation
+
+puts "Innovation is the creative application of scientific knowledge to create new products, processes and services."
+
+# 3. Exploration
+
+puts "Exploration is the search for new knowledge and understanding through experimentation and observation."
